39f0e56025ac55c923988311eddff724eedb9ac5
Nicolas Galvis <nigalviso@unal.edu.co>
2025-04-21 14:19:13 -0500

    Update Ejercicio 1.py

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,1 +1,10 @@
-
+# Calculadora Primitiva, esta toma dos valores reales, un simbolo, y los opera.
+def calculadora(a,b,x):
+    if(x == "+"):
+        return (a+b)
+    if(x == "-"):
+        return (a-b)
+    if(x == "*"):
+        return (a*b)
+    if(x == "/"):
+        return (a/b)
