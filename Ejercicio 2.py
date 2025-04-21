49361d78d130e98afd20b183e575f5f9cc3404dd
Nicolas Galvis <nigalviso@unal.edu.co>
2025-04-21 14:19:50 -0500

    Update Ejercicio 2.py

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -1,1 +1,8 @@
-
+#* Palindrome sin slicing, el programa compara que los 
+#* Valores de las esquinas sean iguales
+#* Y lo hace sucesivamente hasta llegar el valor del medio
+def palindrome(x):
+    for i in range(len(x)//2):
+        if (x[i] != x[len(x)-1-i]):
+            return False
+    return True
