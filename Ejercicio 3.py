4c2d96757d42cec5c77da49669218fda9f6b27f1
Nicolas Galvis <nigalviso@unal.edu.co>
2025-04-21 14:20:31 -0500

    Update Ejercicio 3.py

--- Ejercicio 3.py
+++ Ejercicio 3.py
@@ -1,1 +1,9 @@
-
+#* Detector de primos, divide x por todos los numeros hasta la raiz de la misma
+#* En caso de que su modulo no sea 0, se retorna como primo
+def esprimo(x):
+    if (x < 2):
+        return False
+    for i in range(2,int(x**0.5)+1):
+        if (x%i == 0):
+            return False
+    return True
